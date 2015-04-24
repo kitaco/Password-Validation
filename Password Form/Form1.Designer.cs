1e0b1d97d24bcaf54f3b63a52a06aa70f4350e81
Kiely <kitaco1028@gmail.com>
2015-04-23 20:20:14 -0400

    Password Validation in C#
    Visual C# 2012
    
    Tutorial 8-1

---
+++ Password Form/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Password_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.checkPasswordButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A valid pasword must meet these requirements:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(380, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1 - It must be at least 8 characters long";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(460, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "2 - It must contain at least one uppercase letter ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(452, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "3 - It must contain at least one lowercase letter";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(422, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "4 - It must contain at least one numeric digit";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(171, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(196, 29);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Enter A Password:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(132, 237);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(270, 35);
+            this.passwordTextBox.TabIndex = 6;
+            this.passwordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkPasswordButton
+            // 
+            this.checkPasswordButton.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkPasswordButton.Location = new System.Drawing.Point(92, 288);
+            this.checkPasswordButton.Name = "checkPasswordButton";
+            this.checkPasswordButton.Size = new System.Drawing.Size(174, 49);
+            this.checkPasswordButton.TabIndex = 7;
+            this.checkPasswordButton.Text = "Check Password";
+            this.checkPasswordButton.UseVisualStyleBackColor = true;
+            this.checkPasswordButton.Click += new System.EventHandler(this.checkPasswordButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(272, 288);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(174, 49);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 374);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkPasswordButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Password Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button checkPasswordButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
